cbef1c049f6d0bbf6c2768e466089207bf5c31eb
Annie Chi <annie@Annies-MacBook-Pro.local>
2023-08-18 11:46:12 +0800

    chore: refactor

--- pages/WithStaticImage.js
+++ pages/WithStaticImage.js
@@ -1,23 +1,15 @@
-import Head from "next/head";
 import Image from "next/image";
-import Image1 from "../public/static-image-1.png";
-import Image2 from "../public/static-image-2.png";
-import Image3 from "../public/static-image-3.png";
+import Image1 from "../public/image-1.png";
+import Image2 from "../public/image-2.png";
+import Image3 from "../public/image-3.png";
 
 export default function WithoutStaticImage() {
   return (
     <div>
-      <Head>
-        <title>Without Static Image</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1>WithStaticImage</h1>
-        <Image src={Image1} width={600} height={600} />
-        <Image src={Image2} width={600} height={600} />
-        <Image src={Image3} width={600} height={600} />
-      </main>
+      <h1>WithStaticImage</h1>
+      <Image src={Image1} width={600} height={600} />
+      <Image src={Image2} width={600} height={600} />
+      <Image src={Image3} width={600} height={600} />
     </div>
   );
 }
--- pages/WithoutStaticImage.js
+++ pages/WithoutStaticImage.js
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import Image from "next/image";
 import Image1 from "../assets/image-1.png";
 import Image2 from "../assets/image-2.png";
@@ -7,17 +6,10 @@ import Image3 from "../assets/image-3.png";
 export default function WithoutStaticImage() {
   return (
     <div>
-      <Head>
-        <title>Without Static Image</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1>WithoutStaticImage</h1>
-        <Image src={Image1} width={600} height={600} />
-        <Image src={Image2} width={600} height={600} />
-        <Image src={Image3} width={600} height={600} />
-      </main>
+      <h1>WithoutStaticImage</h1>
+      <Image src={Image1} width={600} height={600} />
+      <Image src={Image2} width={600} height={600} />
+      <Image src={Image3} width={600} height={600} />
     </div>
   );
 }
