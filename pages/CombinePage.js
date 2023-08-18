dac50c5f414148e825930693ae436a6205d9520f
Annie Chi <annie@Annies-MacBook-Pro.local>
2023-08-18 14:23:54 +0800

    feat: add page

---
+++ pages/CombinePage.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Image1 from "../public/image-6.png";
+import Image6 from "../assets/image-1.png";
+
+export default function CombinePage() {
+  return (
+    <div>
+      <h1>CombinePage</h1>
+      <Image src={Image1} width={1000} height={1000} alt="static image" />
+      <Image src={Image6} width={1000} height={1000} alt="not static image" />
+    </div>
+  );
+}
