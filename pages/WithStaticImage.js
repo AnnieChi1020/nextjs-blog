696cc38dffa490bd89f93550ac7ad0a6c69023ed
Annie Chi <annie@Annies-MacBook-Pro.local>
2023-08-18 11:51:29 +0800

    feat: use larger images

--- pages/WithStaticImage.js
+++ pages/WithStaticImage.js
@@ -2,6 +2,8 @@ import Image from "next/image";
 import Image1 from "../public/image-1.png";
 import Image2 from "../public/image-2.png";
 import Image3 from "../public/image-3.png";
+import Image4 from "../public/image-4.png";
+import Image5 from "../public/image-5.png";
 
 export default function WithoutStaticImage() {
   return (
@@ -10,6 +12,8 @@ export default function WithoutStaticImage() {
       <Image src={Image1} width={600} height={600} />
       <Image src={Image2} width={600} height={600} />
       <Image src={Image3} width={600} height={600} />
+      <Image src={Image4} width={600} height={600} />
+      <Image src={Image5} width={600} height={600} />
     </div>
   );
 }
