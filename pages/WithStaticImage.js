15edc237142f5e258ac98cb9594bc902bbfba9e8
Annie Chi <annie@Annies-MacBook-Pro.local>
2023-08-18 10:33:54 +0800

    feat: add pages

---
+++ pages/WithStaticImage.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import Image from "next/image";
+import FImage from "../public/create-course-image.png";
+
+export default function WithoutStaticImage() {
+  return (
+    <div>
+      <Head>
+        <title>Without Static Image</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>WithStaticImage</h1>
+        <Image src={FImage} width={400} height={400} />
+      </main>
+    </div>
+  );
+}
